dc25581417ddcc92c50b0cfef385174aa578050e
muxator <antonio.muci@bancaditalia.it>
2023-07-28 15:21:37 +0200

    arg_parser: fix description of "--generate" parameter
    The description of the "--generate" parameter was copied from "--parallel" by
    mistake.

--- src/arg_parser.hpp
+++ src/arg_parser.hpp
@@ -17,7 +17,7 @@ po::variables_map parse_args(int argc, char *argv[]) {
         graph.add_options()
             ("input-path,i", po::value<std::string>(), "path to file with input graph")
             ("output-path,o", po::value<std::string>(), "path to file to store used graph")
-            ("generate,g", po::bool_switch(), "whether to use serial or parallel algorithm")
+            ("generate,g", po::bool_switch(), "whether to generate a random graph. If given, also pass --v-number and --density")
             ("v-number,v", po::value<unsigned>(), "number of vertices to generate")
             ("density,d", po::value<float>(), "probability of edge existence")
         ;
